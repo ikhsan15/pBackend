fd95c0b3a6dfc9f28d565373f5647dd6d8d025c1
ikhsan15 <ikhsan15nur@gmail.com>
2021-10-23 18:29:40 +0700

    langkah membuat project laravel

---
+++ latihan/pertemuan4/readme.php
@@ -0,0 +1,12 @@
+<?php
+
+// create laravel project
+composer create-project laravel/laravel animals-api
+
+// masuk ke direktori
+
+// run project
+php artisan serve
+
+// create controller
+php artisan make:controller AnimalController
