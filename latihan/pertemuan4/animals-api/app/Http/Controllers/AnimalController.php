e418c22a10fa0f69ad8c571490ee40627fc9bbb2
ikhsan15 <ikhsan15nur@gmail.com>
2021-10-29 10:21:24 +0700

    update function

--- latihan/pertemuan4/animals-api/app/Http/Controllers/AnimalController.php
+++ latihan/pertemuan4/animals-api/app/Http/Controllers/AnimalController.php
@@ -6,13 +6,22 @@ use Illuminate\Http\Request;
 
 class AnimalController extends Controller{
   function index(){
-    echo "ini adalah daftar animals";
+    // echo "ini adalah daftar animals";
+    // return json_encode([
+    //   "name"  => "fish",
+    //   "color" => "black",
+    // ]);
   }
-  function create(){
+  function create(Request $request){
     echo "add new daftar animals";
   }
-  function update($id){
-    echo "update daftar animals dari id $id";
+  function update(Request $request, $id){
+    $name = $request->nama;
+    $color = $request->warna;
+    echo "update daftar animals dari id ".$id."<br>";
+    echo "nama terbaru adalah $name";
+    echo "<br>";
+    echo "dengan warna $color";
   }
   function destroy($id){
     echo "delete daftar animals dari id $id";
