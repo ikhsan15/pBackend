df5b610360f1921a2aae9b3012f48b071ad47b13
ikhsan15 <ikhsan15nur@gmail.com>
2021-11-13 11:53:41 +0700

    tugas pertemuan 6

--- tugas/tugas6/animals-api/app/Http/Controllers/StudentController.php
+++ tugas/tugas6/animals-api/app/Http/Controllers/StudentController.php
@@ -7,104 +7,64 @@ use App\Models\Student; //import model
 
 class StudentController extends Controller{
   function index(){
-    $data = Student::all();
-
-    return response()->json($data, 200);
+    $students = Student::all();
+    if(count($students) == 0){
+      $response = [
+          'message' => 'Data not found'
+      ];
+      return response()->json($response,404);
+    }
+    else{
+      $response = [
+        'message' => "Get All Student",
+        'data' => $students
+      ];
+      return response()->json($response,200);
+    }
   }
 
-  function create(Request $request){
-    // menerima data request dari body
-    $nama     = $request->nama;
-    $nim      = $request->nim;
-    $email    = $request->email;
-    $jurusan  = $request->jurusan;
-    
-    // insert data ke db => students
-    $student = Student::create([
-      'nama'    => $nama,
-      'nim'     => $nim,
-      'email'   => $email,
-      'jurusan' => $jurusan
-    ]);
-
-    $data = [
-      'message' => 'student is created succesfully',
-      'data'    => $student
-    ];
-
-    return response()->json($data, 201);
-  }
-
-  function update(Request $request, $id){
-    $student = Student::find($id);
-    $student->update($request->all());
-
+  public function create(Request $request){
+    $student = Student::create($request->all());
     $response = [
-      'message' => 'student is updated succesfully',
+      'message' => 'Student is created succsesfully',
       'data' => $student
     ];
-
-    return response()->json($response, 200);
+    return response()->json($response,201);
   }
 
-  // isfha style
-  // function update(Request $request, $id){
-  //   // menerima data request dari body
-  //   $nama     = $request->nama;
-  //   $nim      = $request->nim;
-  //   $email    = $request->email;
-  //   $jurusan  = $request->jurusan;
+  public function update(Request $request,$id){
+    $student = Student::find($id);
+    if ($student){
+      $student->update($request->all());
+      $response = [
+        'message' => 'Student updated',
+        'data' => $student
+      ];
+      return response()->json($response,200);
+    }
+    else{
+      $response=[
+        'message' => "Data not found"
+      ];
+      return response()->json($response,404);
+    }
+  }
 
-  //   // cari data student berdasarkan id
-  //   $data = Student::find($id);
-
-  //   if($data){
-  //     $data->update([
-  //       'nama'    => $nama,
-  //       'nim'     => $nim,
-  //       'email'   => $email,
-  //       'jurusan' => $jurusan
-  //     ]);
-
-  //     return response()->json($data, 200);
-  //   }
-  //   else{
-  //     $errorMessage = [
-  //       'message' => 'data not found'
-  //     ];
-  //     return response()->json($errorMessage, 404);
-  //   }
-  // }
-
-  function destroy($id){
+  public function destroy($id){
     $stundent = Student::find($id);
-    $stundent->delete();
-
-    $response = [
-      'message' => 'student is deleted succesfully'
-    ];
-
-    return response()->json($response, 200);
+    if ($stundent){
+      $stundent->delete();
+      $response = [
+        'message' => 'Student deleted'
+      ];
+      return response()->json($response,200);
+    }
+    else{
+      $response = [
+        'message' => "Data not found"
+      ];
+      return response()->json($response,404);
+    }
   }
-
-  // isfha style
-  // function destroy($id){
-  //   $data = Student::find($id);
-
-  //   if($data){
-  //     $data-delete();
-
-  //     $successMessage = [
-  //       'message' => 'deleted succesfully'
-  //     ];
-  //     return response()->json($successMessage, 204);
-  //   }
-  //   else{
-  //     $errorMessage = [
-  //       'message' => 'data not found'
-  //     ];
-  //     return response()->json($errorMessage, 404);
-  //   }
-  // }
 
 }
